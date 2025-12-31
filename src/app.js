c3e0a1c68bafe4f2eb83f20c3b7bd2ea08f9acf4
Sushant Singh <sushant22007@gmail.com>
2025-12-31 13:34:04 +0530

    Add CORS middleware to enable cross-origin requests from the frontend

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import morgan from 'morgan';
 import userRoute from './routes/user.routes.js';
 import { connectDB } from "./config/db.connection.js";
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
