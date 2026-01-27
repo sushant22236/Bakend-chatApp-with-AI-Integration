faa6dc5e5fa20d4e56955d1719d8f360a22daa38
Sushant Singh <sushant22007@gmail.com>
2026-01-27 14:59:31 +0530

    Add authentication middleware for WebSocket connections: verify JWT tokens and handle errors

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import {config} from './src/config/env.js';
 import app from './src/app.js';
 import http from 'http';  
-import { Server } from 'socket.io';  
+import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken';  
 
 const server = http.createServer(app);
-
 const io = new Server(server);
+
+io.use((socket, next) => {
+    try {
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(' ')[1];
+
+        if(!token) {
+            throw new Error('Authentication error: Token not provided');
+        }
+
+        const decoded = jwt.verify(token, config.Jwt_secret);
+
+        if (!decoded) {
+            throw new Error('Authentication error: Invalid token');
+        }
+
+        socket.user = decoded;
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
 io.on('connection', socket => {
 
     console.log('a user connected');
